2afc711b1be5e1528432474d4310db7d4bc24e3a
brandonhach <specialcambo@gmail.com>
2024-03-10 13:06:01 -0400

    removed paddings for box 2x2 and moved to projectBox

--- components/grid-item.tsx
+++ components/grid-item.tsx
@@ -7,7 +7,7 @@ const variants = cva('shadow-grid rounded-3xl bg-base-300 flex flex-col justify-
 		size: {
 			'1x2': 'md:col-span-1 col-span-2 row-span-2 ',
 			'2x1': 'md:col-span-2 col-span-full row-span-1 py-4 md:px-8 px-4',
-			'2x2': 'md:col-span-2 col-span-full row-span-2 md:p-8 p-4',
+			'2x2': 'md:col-span-2 col-span-full row-span-2',
 			'2x4': 'md:col-span-2 col-span-full row-span-4 relative overflow-hidden',
 		},
 	},
