fedd4c5f60dbfe48f032258c626b9f0a73885fd6
brandonhach <specialcambo@gmail.com>
2024-03-10 13:07:38 -0400

    refactored to display hover effect if link exist within item

--- components/grid-items/pictureBox.tsx
+++ components/grid-items/pictureBox.tsx
@@ -1,22 +1,32 @@
 import { GridItemInterface } from '@/config/site-config';
 import Image from 'next/image';
+import Link from 'next/link';
 
 const PictureBox = ({ item }: { item: GridItemInterface }) => {
 	const videoSrc = `/video/${item.video}`;
 	const src = `/image/${item.image}`;
+
+	const content = item.image ? (
+		<Image className='object-cover w-full h-full' src={src} alt='' fill />
+	) : (
+		<video
+			autoPlay
+			loop
+			muted
+			controlsList='nodownload noplaybackrate nofullscreen'
+			className='z-0 object-cover w-full h-full'>
+			<source src={videoSrc} type='video/mp4' />
+		</video>
+	);
+
 	return (
 		<div className='relative w-full h-full overflow-hidden rounded-3xl'>
-			{item.image ? (
-				<Image className='object-cover w-full h-full' src={src} alt='' fill />
+			{item.link ? (
+				<Link href={item.link} target='_blank'>
+					{content}
+				</Link>
 			) : (
-				<video
-					autoPlay
-					loop
-					muted
-					controlsList='nodownload noplaybackrate nofullscreen'
-					className='z-0 object-cover w-full h-full'>
-					<source src={videoSrc} type='video/mp4' />
-				</video>
+				content
 			)}
 		</div>
 	);
